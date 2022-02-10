81c949f12c5b3a973ee78675d3633d3e3c7f1b4d
ericchi00 <ericchi00@gmail.com>
2022-02-10 13:36:02 -0800

    fixed logic and added reset buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector('.gridContainer');
 
-//creates the grid based on the num (num x num format)
+//creates the grid based on the number input (num x num format)
 function gridCreator(num) {
     let gridSize = num * num;
     for (i = 1; i <= gridSize; i++) {
@@ -11,20 +11,53 @@ function gridCreator(num) {
     }
 }  
 
+//deletes the current grid
 function resetGrid() {
-    const oldGrid = document.querySelectorAll('.grid');
-    oldGrid.forEach(grid => oldGrid.remove());
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
 }
 
-const slider = document.getElementById('gridSlider');
-let updateGridSize = document.getElementById('gridSizing');
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', () => {
+    resetGrid();    
+    gridCreator(15);
+    slider.value = 15;
+    update();
+    hoverEffect();
+});
 
-let update = () => updateGridSize.innerHTML = slider.value;
+const resetColorBtn = document.querySelector('#resetColor');
+resetColorBtn.addEventListener('click', () => {
+    let gridClass = document.querySelectorAll('.grid');
+    gridClass.forEach(div => {
+        div.setAttribute("style", "background-color: white;")
+    })
+})
+
+
+const slider = document.getElementById('gridSlider');
+const updateGridSize = document.getElementById('gridSizing');
+
+//resets grid and then updates it to the new one on slier
+let update = () => updateGridSize.textContent = slider.value + ' x ' + `${slider.value}`;
 slider.addEventListener('input', () => {
     resetGrid();
     update();
     gridCreator(slider.value);
+    hoverEffect();
 });
 
-update();
-gridCreator(slider.value);
+update(); //sets the text to the inital grid value (10x10)
+gridCreator(slider.value); //creates the inital grid (10x10)
+hoverEffect(); //allows the colors to appear on the inital grid
+
+//adds hover effect
+function hoverEffect() {
+    let gridClass = document.querySelectorAll('.grid');
+    gridClass.forEach(div => {
+        div.addEventListener('mouseover', () => {
+            div.setAttribute("style", "background-color: #c4dfff;")
+        });
+    });
+}
